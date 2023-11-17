8aa899f254e15dd8cb188e5e784d7e94d0b6b9a6
Sushmita <135802180+Sushmita013@users.noreply.github.com>
2023-11-17 20:23:56 +0530

    Haircut animations, massage room anim changes

--- Assets/Scripts/Masseuse.cs
+++ Assets/Scripts/Masseuse.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 using DG.Tweening;
 
 public class Masseuse : MonoBehaviour
@@ -23,12 +24,15 @@ public class Masseuse : MonoBehaviour
     public Transform chairPoint;
     public Transform chairPoint1;
 
+    public NavMeshAgent boy;
+    public ParticleSystem hairSplash;
+
     void Start()
     {
         //parent = GetComponent<Animator>();
         instance = this;
         keepBabyButton.onClick.AddListener(() => StartCoroutine(Action4()));
-        massageButton.onClick.AddListener(() => StartCoroutine(Massage()));
+        massageButton.onClick.AddListener(() => StartCoroutine(Haircut()));
         //takingBabyButton.onClick.AddListener(() => StartCoroutine(Sit()));
 
     } 
@@ -38,35 +42,81 @@ public class Masseuse : MonoBehaviour
         babyGO.SetActive(true); 
         PlayAnimationParent("Keeping baby on table");
         PlayAnimationBaby("keep baby on table");
-        yield return new WaitForSeconds(7);
-        diaperGO.SetActive(false);
+        yield return new WaitForSeconds(4f);
+        //diaperGO.SetActive(false);
         babyGO.transform.DOScale(new Vector3(3f, 3f, 3f), 0.1f);
-        babyGO.transform.localPosition = new Vector3(32.5f, -0.396f, 21.5f);
+        babyGO.transform.localPosition = new Vector3(32.5f, -0.586f, 20.2f);
         //girlGO.SetActive(false);
-        yield return new WaitForSeconds(2f);
-        PlayAnimationParent("standing idle");
+        //yield return new WaitForSeconds(2f);
+        //PlayAnimationParent("standing idle");
         //StartCoroutine(Action5());
+        PlayAnimationParent("walking");
         PlayAnimationBaby("baby on table idle");
+        PlayAnimationMassage("Taking oil");
+        boy.SetDestination(chairPoint.position);
+        yield return new WaitForSeconds(1.25f);
+        girlGO.transform.DOLocalRotate(new Vector3(0, 90, 0), 0.1f);
+        PlayAnimationParent("Stand to sit");
+        yield return new WaitForSeconds(2.5f);
+        PlayAnimationParent("Sitting idle without baby");
         PlayAnimationMassage("Massage");
-        yield return new WaitForSeconds(20);
-        PlayAnimationMassage("standing idle"); 
+        yield return new WaitForSeconds(15);
+        PlayAnimationParent("Sit to stand");
+        yield return new WaitForSeconds(1.5f);
+        PlayAnimationParent("walking"); 
+        boy.SetDestination(chairPoint1.position);
+        yield return new WaitForSeconds(1.5f); 
+        PlayAnimationMassage("standing idle");
+        girlGO.transform.DORotate(new Vector3(0, 90, 0), 0.1f); 
         ////girlGO.SetActive(true);
         diaperGO.SetActive(true);
         babyGO.transform.DOScale(new Vector3(2f, 2f, 2f), 0.1f);
         babyGO.transform.localPosition = new Vector3(32.757f, 0.203f, 20.75f);
         PlayAnimationParent("taking baby from table");
-        PlayAnimationBaby("take baby from table"); 
-        yield return new WaitForSeconds(10);
-        gameObject.transform.DORotate(new Vector3(0, 40, 0), 0.1f);
-        gameObject.transform.position = new Vector3(-69.3f, -8.08f, 17.3f); 
+        PlayAnimationBaby("take baby from table");
+        yield return new WaitForSeconds(4);
+        //gameObject.transform.DORotate(new Vector3(0, -90, 0), 0.1f);
+        //gameObject.transform.position = new Vector3(-69.3f, -8.08f, 17.3f);
+        babyGO.transform.localPosition = new Vector3(32.532f, 0.203f, 21); 
         PlayAnimationParent("standing idle with baby");
-        PlayAnimationBaby("standing idle"); 
+        PlayAnimationBaby("standing idle");
 
     }
 
-    public IEnumerator Massage()
+    public IEnumerator Haircut()
     {
-        yield return new WaitForSeconds(3); 
+        yield return new WaitForSeconds(3);
+        PlayAnimationParent("keeping baby on chair");
+        PlayAnimationBaby("Keeping baby on chair");
+        yield return new WaitForSeconds(4f);
+        PlayAnimationBaby("baby on chair idle");
+        PlayAnimationParent("walking");
+        PlayAnimationMassage("Haircut"); 
+        boy.SetDestination(chairPoint.position);
+        hairSplash.Play();
+        hairSplash.playbackSpeed = 0.25f;
+        yield return new WaitForSeconds(3f);
+        girlGO.transform.DOLocalRotate(new Vector3(0, -180, 0), 0.1f);
+        PlayAnimationParent("Stand to sit");
+        //hairSplash.Play(); 
+        yield return new WaitForSeconds(1.5f); 
+        PlayAnimationParent("Sitting idle without baby");
+        //hairSplash.Play(); 
+        yield return new WaitForSeconds(15);
+        PlayAnimationParent("Sit to stand");
+        //hairSplash.Play(); 
+        yield return new WaitForSeconds(1.5f);
+        PlayAnimationParent("walking");
+        boy.SetDestination(chairPoint1.position);
+        yield return new WaitForSeconds(3);
+        hairSplash.Stop(); 
+        PlayAnimationMassage("standing idle"); 
+        girlGO.transform.DORotate(new Vector3(0, 0, 0), 0.1f); 
+        PlayAnimationParent("taking baby from chair");
+        PlayAnimationBaby("baby going with parent from chair");
+        yield return new WaitForSeconds(4);
+        PlayAnimationParent("standing idle with baby");
+        PlayAnimationBaby("standing idle");
     }
     public IEnumerator Action5()
     { 
@@ -78,8 +128,7 @@ public class Masseuse : MonoBehaviour
         girlGO.transform.DOLocalRotate(new Vector3(0, -230, 0), 0.1f);
         yield return new WaitForSeconds(0.1f);
         PlayAnimationParent("Stand to sit");
-        yield return new WaitForSeconds(2);
-
+        yield return new WaitForSeconds(2); 
 
     }
 
